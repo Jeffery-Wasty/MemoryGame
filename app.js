aaaf6dc4d702cd650bab90e345bedb5fddaecae9
Jeffery Wasty <jeff.wasty@gmail.com>
2019-10-31 12:19:46 -0700

    Thank you Andy

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes/game');
+
+mongoose.connect(
+  'mongodb+srv://jeffw:uP3DZBaGTkTVg8N@cluster0-eqnvp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
+
+app
+  .set('view engine', 'pug')
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(routes)
+  .listen(3005);
